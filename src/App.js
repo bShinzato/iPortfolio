865173b9e6625af3583c749fbc08569416945e43
bShinzato <bshinzato@outlook.com>
2023-06-28 12:29:18 -0700

    app.css,.js,index.js added first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/NavBar.js'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import adventures from './components/pages/Adventures';
+import family from './components/pages/Family';
+import physicalFitness from './components/pages/PhysicalFitness';
+import hobbies from './components/pages/Hobbies';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+       <Route path='/' exact Component={Home}/>
+       <Route path='/adventures' exact Component={adventures}/>
+       <Route path='/family' exact Component={family}/>
+       <Route path='/physicalFitness' exact Component={physicalFitness}/>
+       <Route path='/hobbies' exact Component={hobbies}/>
+      </Routes>
+    </Router>
+     
+    </>
   );
 }
 
