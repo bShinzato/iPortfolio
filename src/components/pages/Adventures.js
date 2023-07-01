2933373d62ae3c4359b62d91e6fe21670af7eff1
bShinzato <bshinzato@outlook.com>
2023-06-30 23:16:01 -0700

    carousel push.

--- src/components/pages/Adventures.js
+++ src/components/pages/Adventures.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Zoom } from "react-slideshow-image";
+import image3 from "../../imagesforcarousel/aimg-3.jpg";
+import image4 from "../../imagesforcarousel/aimg-4.jpg";
+import image5 from "../../imagesforcarousel/aimg-5.png";
+import '../CarouselStyle.css'
 
+const images = [image3, image4, image5];
 
-const Adventures = () => {
+const zoomOutProperties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  scale: 0.4,
+  arrows: true
+};
+
+const Slideshow = () => {
+  return (
+    <div className="slide-container">
+      <Zoom {...zoomOutProperties}>
+        {images.map((each, index) => (
+          <img key={index} style={{ width: "100%" }} src={each} />
+        ))}
+      </Zoom>
+    </div>
+  );
+};
+
+function Adventures() {
   return (
     <>
-    <h1>Hello there these are my adventures.</h1>
+    <h1>Some Pictures Of My Traveling</h1>
+    <div className="carousel">
+      <Slideshow />
+    </div>
     </>
-
-  )
+  );
 }
 
-export default Adventures
+export default Adventures;
