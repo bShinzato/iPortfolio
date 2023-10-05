7cb493fbbf51017569bae076dafe5216a8ea9f0d
Brandon Shinzato <bshinzato@outlook.com>
2023-10-05 15:01:26 -0700

    changed skills and CSS for aboutme page.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,23 +7,54 @@ const Skills = () => {
   return (
     <div className="SkillsContainer">
       <div className="SkillsCards">
-        <div className="SkillCard TechnicalSkills">
-          <h3>Technical Skills</h3>
+        <div className="SkillCard Skills">
+          <h3>Languages and Frameworks</h3>
           <ul>
             <li>JavaScript</li>
+            <li>C#</li>
             <li>HTML</li>
             <li>CSS</li>
-            <li>Ruby</li>
-            <li>Git</li>
-            <li>PostgreSQL</li>
-            <li>Postman</li>
-            <li>React</li>
-            <li>Ruby on Rails</li>
-            <li>Cypress</li>
             <li>Tailwind</li>
+            <li>Ruby</li>
+            <li>React</li>
+            <li>.NET</li>
+            <li>Ruby on Rails</li>
           </ul>
         </div>
-        <div className="SkillCard SoftSkills">
+        <div className="SkillCard Skills">
+          <h3>Testing</h3>
+          <ul>
+            <li>Jest</li>
+            <li>RSpec</li>
+            <li>Cypress</li>
+            <li>E2E Testing</li>
+            <li>Unit Testing</li>
+            <li>Integration Testing</li>
+          </ul>
+        </div>
+        <div className="SkillCard Skills">
+          <h3>Technologies</h3>
+          <ul>
+            <li>Devise</li>
+            <li>auth0</li>
+            <li>PGAdmin</li>
+            <li>Postman</li>
+            <li>Git</li>
+            <li>Github</li>
+            <li>PostgreSQL</li>
+          </ul>
+        </div>
+        <div className="SkillCard Skills">
+          <h3>General</h3>
+          <ul>
+            <li>Miro</li>
+            <li>Linear</li>
+            <li>Trello</li>
+            <li>Figma</li>
+            <li>Canva</li>
+          </ul>
+        </div>
+        <div className="SkillCard Skills">
           <h3>Soft Skills</h3>
           <ul>
             <li>Time Management</li>
