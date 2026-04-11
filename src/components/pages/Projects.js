3cccf6466a9185715244d4833ed9643a58232192
Brandon Shinzato <131624991+bShinzato@users.noreply.github.com>
2026-04-11 15:49:54 -0500

    fixed problem with another project within this one. added the project to project page.

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -8,7 +8,6 @@ const Projects = () => {
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-           
             <CardItem
               src="https://static.vecteezy.com/system/resources/previews/070/513/421/non_2x/cute-pixel-monster-waving-hello-free-vector.jpg"
               text="Rogue-lite game that involves minimal clicking. Can buy potions or a sword to slay monsters. "
@@ -17,6 +16,17 @@ const Projects = () => {
               target="_blank"
             />
           </ul>
+
+          <ul className="cards__items">
+            <CardItem
+              src="https://static.vecteezy.com/system/resources/previews/070/513/421/non_2x/cute-pixel-monster-waving-hello-free-vector.jpg"
+              text="Rogue-lite game that involves minimal clicking. Can buy potions or a sword to slay monsters. "
+              label="MiniGame"
+              path="https://basic-movement-game.vercel.app"
+              target="_blank"
+            />
+          </ul>
+
         </div>
       </div>
     </div>
